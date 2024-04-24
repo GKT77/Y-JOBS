1d70bc5c3766e4c8f7c6eca1be61b12729adee1a
GKT77 <167949858+GKT77@users.noreply.github.com>
2024-04-24 20:49:09 +0000

    Added dynamic data and API  route

--- app.py
+++ app.py
@@ -1,7 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, jsonify
 app =  Flask (__name__)
+JOBS = [
+  {
+    'id' : 1,
+    'title' : 'Data Analyst',
+     'location' : 'Toronto, Canda',
+    'salary': 'CAD $100,000'
+  },
+  {
+    'id' : 2,
+    'title' : 'Java Developer',
+     'location' : 'Calgary, Canda',
+    'salary': 'CAD $110,000'
+  },
+{
+  'id' : 3,
+  'title' : 'DevOps Engineer',
+   'location' : 'Remote',
+ 
+},
+{
+  'id' : 4,
+  'title' : 'Test automation Engineer',
+   'location' : 'Toronto, Canda',
+  'salary': 'CAD $95,000'
+},
+{
+  'id' : 5,
+  'title' : 'Test Engineer',
+   'location' : 'Vancouer, Canda',
+  'salary': 'CAD $100,000'
+},
+{
+  'id' : 6,
+  'title' : 'Salesforce Developer',
+   'location' : 'Toronto, Canda',
+  'salary': 'CAD $115,000'
+},
+  {
+    'id' : 7,
+    'title' : 'Front end Developer',
+     'location' : 'Montreal, Canda',
+    'salary': 'CAD $120,000'
+  }
+]
 @app.route("/")
 def hello_Y():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='Y')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
